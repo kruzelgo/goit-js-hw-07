53906efc0a75399a77756d0966a69c5c8459acf9
kruzelgo <kruzelgo@gmail.com>
2024-01-26 21:09:15 +0100

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,15 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const imagesGallery = document.querySelector(".gallery");
-console.log(galleryItems);
+
+//  .map(
+//     (image) => `
+//       <div class="gallery__item">
+//         <img src="${image.url}" alt="${image.alt}">
+//       </div>
+//     `
+//   )
+//   .join("");
+
+// listGallery.insertAdjacentHTML("beforeend", galleryHTML);
+// console.log(galleryItems);
