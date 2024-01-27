d59e5809507d5a5b6d917a9af143d07212e909a2
kruzelgo <kruzelgo@gmail.com>
2024-01-27 16:51:34 +0100

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,14 +2,49 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 const imagesGallery = document.querySelector(".gallery");
 
-//  .map(
-//     (image) => `
-//       <div class="gallery__item">
-//         <img src="${image.url}" alt="${image.alt}">
-//       </div>
-//     `
-//   )
-//   .join("");
+const gallery = galleryItems.map((item) => {
+  const galleryList = document.createElement("li");
+  const link = document.createElement("a");
+  const image = document.createElement("img");
 
-// listGallery.insertAdjacentHTML("beforeend", galleryHTML);
-// console.log(galleryItems);
+  galleryList.classList.add("gallery__item");
+  link.classList.add("gallery__link");
+  image.classList.add("gallery__image");
+
+  link.href = item.original;
+  image.src = item.preview;
+  image.dataset.source = item.original;
+  image.alt = item.description;
+
+  link.appendChild(image);
+  galleryList.appendChild(link);
+  return galleryList;
+});
+
+imagesGallery.append(...gallery);
+
+let lightboxInstance;
+
+imagesGallery.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.nodeName === "IMG") {
+    lightboxInstance = basicLightbox.create(
+      `<img src="${event.target.dataset.source}" alt="${event.target.alt}" width="800" height="600">`,
+      {
+        onShow: (instance) => {
+          document.addEventListener("keydown", onEscape);
+        },
+        onClose: (instance) => {
+          document.removeEventListener("keydown", onEscape);
+        },
+      }
+    );
+    lightboxInstance.show();
+  }
+});
+
+function onEscape(event) {
+  if (event.key === "Escape" && lightboxInstance) {
+    lightboxInstance.close();
+  }
+}
